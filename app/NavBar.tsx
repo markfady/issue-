ed565ffaeaf027b0396130ca990c9079c6da2cde
Markfady <markfady9@gmail.com>
2024-01-15 13:27:31 +0200

    Build NavBar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,13 +1,17 @@
 import Link from 'next/link'
 import React from 'react'
-
+import {AiFillBug} from 'react-icons/ai'
 const NavBar = () => {
+  const links=[ //To avoid re write the Li and Links 
+    {label:'Dashboard',href:'/'},
+    {label:'Issues',href:'/issues'}
+  ]
   return (
-    <nav className='flex space-x-6' >
-        <Link href='/'>Logo</Link>
-        <ul className='flex'>
-            <li><Link href="/">DashBoard</Link></li>
-            <li><Link href="/">Issues</Link></li>
+    <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center' >
+        <Link href='/'><AiFillBug/></Link>
+        <ul className='flex space-x-6'>
+          {links.map(link=><Link key={link.href} className='text-zinc-500
+           hover:text-zinc-800 transition-colors' href={link.href}>{link.label}</Link>)}
         </ul>
     </nav>
   )
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import NavBar from './navBar'
+import NavBar from './NavBar'
 
 const inter = Inter({ subsets: ['latin'] })
 
