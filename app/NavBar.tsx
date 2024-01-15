c77745958247636403d87039f962672fdbfa7c2a
Markfady <markfady9@gmail.com>
2024-01-15 13:49:32 +0200

    NavBar ClassName,Issues page

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,7 +1,11 @@
+'use client'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 import {AiFillBug} from 'react-icons/ai'
+import classNames from 'classnames'
 const NavBar = () => {
+  const currentPath=usePathname(); //When use Hook make use client at the top 
   const links=[ //To avoid re write the Li and Links 
     {label:'Dashboard',href:'/'},
     {label:'Issues',href:'/issues'}
@@ -10,8 +14,11 @@ const NavBar = () => {
     <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center' >
         <Link href='/'><AiFillBug/></Link>
         <ul className='flex space-x-6'>
-          {links.map(link=><Link key={link.href} className='text-zinc-500
-           hover:text-zinc-800 transition-colors' href={link.href}>{link.label}</Link>)}
+          {links.map(link=><Link key={link.href} className={classNames({ //Use classNames to don't manipulate tirnary in HTML code
+            'text-zinc-900':link.href===currentPath,
+            'text-zinc-500':link.href!==currentPath,
+            'hover:text-zinc-800 transition-colors':true //Default at all cases do this style
+          })} href={link.href}>{link.label}</Link>)}
         </ul>
     </nav>
   )
