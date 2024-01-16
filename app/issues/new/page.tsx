86127e62ab87c368f66a6b9d01506f281d398236
Markfady <markfady9@gmail.com>
2024-01-16 15:59:25 +0200

    Add a markdown editor

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,14 +1,15 @@
 'use client'
 import { Button, TextArea, TextField } from '@radix-ui/themes'
 import React from 'react'
-
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
 const NewIssue = () => {
   return (
     <div  className='max-w-xl space-y-3'>
     <TextField.Root>  {/* Form Must be use client  */}
     <TextField.Input placeholder="Title" />
   </TextField.Root>
-  <TextArea placeholder="Description" />
+  <SimpleMDE placeholder="Description" /> {/*Markdown editor replaces TextArea of radixUI */}
   <Button>Submit New Issue</Button>
     </div>
 
