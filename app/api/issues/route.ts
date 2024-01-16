70bac568debad9ffd4e22fbafb458f7de2b2e30e
Markfady <markfady9@gmail.com>
2024-01-16 22:13:23 +0200

    Implement Client side validation

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,19 +1,16 @@
-
 import { NextRequest, NextResponse } from "next/server";
-import {z} from 'zod'
 import prisma from "@/prisma/client";
-const  createIssueSchema=z.object({ //define the shape of body of the request
-    title:z.string().min(1,'Title is required').max(255),
-    description:z.string().min(1,'description is required')
-})
-export async function POST(request:NextRequest){
-  const body=  await request.json();
- const validation= createIssueSchema.safeParse(body)
- if(!validation.success)
-    return NextResponse.json(validation.error.format(),{status:400}) /*use format() to make it simpler in error handler */
- 
- const newIssue= await prisma.issue.create({
-    data:{title: body.title,description: body.description}
- })
- return NextResponse.json(newIssue,{status:201})
+import { createIssueSchema } from "../../validationSchemas";
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  const validation = createIssueSchema.safeParse(body);
+  if (!validation.success)
+    return NextResponse.json(validation.error.format(), {
+      status: 400,
+    }); /*use format() to make it simpler in error handler */
+
+  const newIssue = await prisma.issue.create({
+    data: { title: body.title, description: body.description },
+  });
+  return NextResponse.json(newIssue, { status: 201 });
 }
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,50 +1,62 @@
-'use client'
-import { Button, Callout, TextArea, TextField } from '@radix-ui/themes'
-import React, { useState } from 'react'
+"use client";
+import { Button, Callout, Text, TextArea, TextField } from "@radix-ui/themes";
+import React, { useState } from "react";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
-import { useForm ,Controller} from 'react-hook-form';
-import axios from 'axios';
-import { useRouter } from 'next/navigation';
-interface IssueForm{
-  title:string;
-  description:string;
-}
+import { useForm, Controller } from "react-hook-form";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createIssueSchema } from "@/app/validationSchemas";
+import { z } from "zod";
+type IssueForm = z.infer<typeof createIssueSchema>; //to prevent redundancy
+
 const NewIssue = () => {
   const router = useRouter();
-  const [error,setError]=useState(''); //State handle error of form
-  const {register,control,handleSubmit}=useForm<IssueForm>()
+  const [error, setError] = useState(""); //State handle error of form
+  const { register, control, handleSubmit,formState:{errors} } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema), //Integrate react form with zod
+  });
+
   return (
-    <div className='max-w-xl '>
-      {error&&<Callout.Root color='red' className='mb-5'>
-  <Callout.Text>
- {error}  {/*view error state in radix UI */}
-  </Callout.Text>
-</Callout.Root>}
-    <form  className=' space-y-3'
-    onSubmit={handleSubmit(async(data)=>{
-    try { //Try,catch to handle error of axios
-      await axios.post('/api/issues',data);
-      router.push('/issues')
-    } catch (error) {
-      setError('unExpected Error occurred'); //set error in state
-    }
-
-    }
-   )}>
-    <TextField.Root>  {/* Form Must be use client  */}
-    <TextField.Input placeholder="Title" {...register('title')}/>
-  </TextField.Root>
-  {/*Markdown editor replaces TextArea of radixUI */}
-  <Controller
-  name='description'
-  control={control}
-  render={({field})=><SimpleMDE placeholder='Description' {...field}/> } 
-  /> 
-  <Button>Submit New Issue</Button>
-    </form>
+    <div className="max-w-xl ">
+      {error && (
+        <Callout.Root color="red" className="mb-5">
+          <Callout.Text>
+            {error} {/*view error state in radix UI */}
+          </Callout.Text>
+        </Callout.Root>
+      )}
+      <form
+        className=" space-y-3"
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            //Try,catch to handle error of axios
+            await axios.post("/api/issues", data);
+            router.push("/issues");
+          } catch (error) {
+            setError("unExpected Error occurred"); //set error in state
+          }
+        })}
+      >
+        <TextField.Root>
+          {/* Form Must be use client*/}
+          <TextField.Input placeholder="Title" {...register("title")} />
+        </TextField.Root>
+        {errors.title&&<Text color="red"  as="p">{errors.title.message}</Text>}
+        {/*Markdown editor replaces TextArea of radixUI */}
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE placeholder="Description" {...field} />
+          )}
+        />
+                {errors.description &&<Text color="red" as="p">{errors.description.message}</Text>}
+        <Button>Submit New Issue</Button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default NewIssue
+export default NewIssue;
