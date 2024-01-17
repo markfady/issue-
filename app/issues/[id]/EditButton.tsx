d7643f822d5415a822cbf4739a9ef9a40e9581c8
Markfady <markfady9@gmail.com>
2024-01-17 19:00:20 +0200

    Single Responsibility principle Issue Detail Page

---
+++ app/issues/[id]/EditButton.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Button } from '@radix-ui/themes'
+import { Pencil2Icon } from '@radix-ui/react-icons'
+import Link from 'next/link'
+const EditButton = ({id}:{id:number}) => {
+  return (
+    <Button>
+    <Pencil2Icon/>
+    <Link href={`/issues/${id}/edit`}>Edit Issue</Link>
+  </Button>
+  )
+}
+
+export default EditButton
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,12 +1,10 @@
-import IssueStatusBadge from '@/app/components/IssueStatusBadge'
 import prisma from '@/prisma/client'
-import { Box, Button, Card, Flex, Grid, Heading, Text } from '@radix-ui/themes'
+import { Box,Grid } from '@radix-ui/themes'
 import { notFound } from 'next/navigation'
 import React from 'react'
-import ReactMarkdown from 'react-markdown'
 import delay from 'delay'
-import { Pencil2Icon } from '@radix-ui/react-icons'
-import Link from 'next/link'
+import IssueDetail from './IssueDetail'
+import EditButton from './EditButton'
 interface Props{
     params:{id:string}
 }
@@ -22,21 +20,10 @@ const IssueDetailPage = async ({params}:Props) => {
 <>
     <Grid columns={{initial:'1',md:'2'}} gap='5'>
     <Box>
-    <Heading>{Issue.title}</Heading>
-    <Flex gap='5' my='3'>
-    <IssueStatusBadge status={Issue.status}/>
-    <Text>{Issue.createdAt.toDateString()}</Text>
-    </Flex>
-    <Card className='prose' mt='4'>
-    <ReactMarkdown>{Issue.description}</ReactMarkdown>
-    </Card>
+    <IssueDetail issue={Issue}/>
     </Box>
     <Box>
-      <Button>
-        <Pencil2Icon/>
-        <Link href={`/issues/${Issue.id}/edit`}>Edit Issue </Link>
-      </Button>
-      
+    <EditButton id={Issue.id} />
     </Box>
     </Grid>
 </>
