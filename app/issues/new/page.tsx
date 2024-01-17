4f8b8baa04df2fc5db4847b73c848980e648ca3e
Markfady <markfady9@gmail.com>
2024-01-17 11:39:54 +0200

    Handle Error Message and Spinner

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -9,8 +9,8 @@ import { useRouter } from "next/navigation";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { createIssueSchema } from "@/app/validationSchemas";
 import { z } from "zod";
-import ErrorMessage from "@/app/components/errorMessage";
-import Spinner from "@/app/components/spinner";
+import ErrorMessage from "@/app/components/ErrorMessage";
+import Spinner from "@/app/components/Spinner";
 type IssueForm = z.infer<typeof createIssueSchema>; //to prevent redundancy
 
 const NewIssue = () => {
