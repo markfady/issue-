9c942a1c17549386298b43e64ee2123bc4c39dde
Markfady <markfady9@gmail.com>
2024-01-19 13:47:18 +0200

    Show a confirmation dialog box

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,12 +1,34 @@
-import { Issue } from '@prisma/client'
-import { Button } from '@radix-ui/themes'
+'use client'
+import { AlertDialog, Button, Flex } from '@radix-ui/themes'
 import React from 'react'
 
 const DeleteIssueButton = ({issueId}:{issueId:number}) => {
   return (
-        <Button color='red' >
-            Delete Issue
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color='red' >Delete Issue</Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content>
+        <AlertDialog.Title>
+          Confirm Deletion
+        </AlertDialog.Title>
+        <AlertDialog.Description>
+          Are you sure you want to delete this Issue?
+        </AlertDialog.Description>
+        <Flex gap="3" mt="4" justify="end">
+        <AlertDialog.Cancel>
+        <Button variant="soft" color="gray">
+          Cancel
         </Button>
+      </AlertDialog.Cancel>
+      <AlertDialog.Action>
+        <Button variant="solid" color="red">
+          Delete Issue
+        </Button>
+      </AlertDialog.Action>
+</Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
   )
 }
 
