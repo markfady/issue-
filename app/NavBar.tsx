725232281ce10da7b5de23770c8bd2c90b21ea1e
Markfady <markfady9@gmail.com>
2024-01-20 12:12:24 +0200

    Refactor the navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -8,56 +8,67 @@ import { useSession } from 'next-auth/react'
 import { Avatar, Box, Container, DropdownMenu, Flex, Text } from '@radix-ui/themes'
 
 const NavBar = () => {
-  const currentPath=usePathname(); //When use Hook make use client at the top 
- const{status,data:session}= useSession() //Make AuthProvider component to use this Hook
- console.log(session?.user?.email)
-  const links=[ //To avoid re write the Li and Links 
-    {label:'Dashboard',href:'/'},
-    {label:'Issues',href:'/issues/list'}
-  ]
+
+
+
+
   return (
     <nav className='border-b mb-5 px-5 py-3' >
       <Container>
       <Flex justify='between'>
         <Flex align='center' gap='3'>
         <Link href='/'><AiFillBug/></Link>
-        <ul className='flex space-x-6'>
-          {links.map(link=>
-          <li  key={link.href}> 
-          <Link  className={classNames({ //Use classNames to don't manipulate tirnary in HTML code
-            'text-zinc-900':link.href===currentPath,
-            'text-zinc-500':link.href!==currentPath,
-            'hover:text-zinc-800 transition-colors':true //Default at all cases do this style
-          })} href={link.href}>{link.label}
-          </Link> </li>)}
-        </ul>
+    <NavLinks/>
         </Flex>
-        <Box>
-    
-          {status==="authenticated"&&
-          (
-            <DropdownMenu.Root>
-              <DropdownMenu.Trigger>
-                <Avatar src={session?.user?.image!} fallback="?" size='2' radius='full' className='cursor-pointer'/>
-              </DropdownMenu.Trigger>
-              <DropdownMenu.Content>
-                <DropdownMenu.Label>
-                  <Text size="2">
-                  {session?.user?.email}
-                  </Text>
-                </DropdownMenu.Label>
-                <DropdownMenu.Label>
-              <Link href='/api/auth/signout'>Logout</Link>
-                </DropdownMenu.Label>
-              </DropdownMenu.Content>
-            </DropdownMenu.Root>
-          )}
-          {status==="unauthenticated"&&(<Link href='/api/auth/signin'>Login</Link>)}
-        </Box>
+        <AuthStatus/>
       </Flex>
       </Container>
     </nav>
   )
+}
+const NavLinks=()=>{
+  const currentPath=usePathname(); //When use Hook make use client at the top 
+  const links=[ //To avoid re write the Li and Links 
+  {label:'Dashboard',href:'/'},
+  {label:'Issues',href:'/issues/list'}
+]
+return (
+  <ul className='flex space-x-6'>
+  {links.map(link=>
+  <li  key={link.href}> 
+  <Link  className={classNames({ //Use classNames to don't manipulate tirnary in HTML code
+    '!text-zinc-900':link.href===currentPath, //! to override the na-link class make dashboard dark not grey
+    'nav-link':true   //Default at all cases do this style
+  })} href={link.href}>{link.label}
+  </Link> </li>)}
+</ul>
+)
+
+}
+const AuthStatus=()=>{
+  const{status,data:session}= useSession() //Make AuthProvider component to use this Hook
+    if(status==='loading') return null
+    if(status==='unauthenticated')  return  <Link href='/api/auth/signin' className='nav-link'>Login</Link>
+  return(
+    <Box>
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger>
+          <Avatar src={session?.user?.image!} fallback="?" size='2' radius='full' className='cursor-pointer'/>
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Content>
+          <DropdownMenu.Label>
+            <Text size="2">
+            {session?.user?.email}
+            </Text>
+          </DropdownMenu.Label>
+          <DropdownMenu.Label>
+        <Link href='/api/auth/signout'>Logout</Link>
+          </DropdownMenu.Label>
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
+  </Box>
+  )
+
 }
 
 export default NavBar
