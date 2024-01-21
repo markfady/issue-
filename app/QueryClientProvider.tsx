e9c75c387f909d4e8c04794c16d5c2eb27d3c183
Markfady <markfady9@gmail.com>
2024-01-21 11:21:04 +0200

    Set up React Query

--- app/QueryClientProvider.tsx
+++ app/QueryClientProvider.tsx
@@ -2,7 +2,7 @@
 import {QueryClient,QueryClientProvider as ReactQueryClientProvider} from '@tanstack/react-query'
 import { PropsWithChildren } from 'react';
 const queryClient=new QueryClient(); //contain cache for storing data came from backend
-const QueryClientProvider = ({children}:PropsWithChildren) => { //Must be used and don't depend on useEffect data to check backend and the response you want to make
+const QueryClientProvider = ({children}:PropsWithChildren) => { //used to get data from backend and cache them in the client so we don't need to fetch them every time
   return (
        <ReactQueryClientProvider client={queryClient}>  {/*this uses react context so it can share QueryClient to our component tree */}
         {children}
