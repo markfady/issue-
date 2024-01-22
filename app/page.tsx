302b98ef117141ea42c2a7cb0aeeeb0d62ce2656
Markfady <markfady9@gmail.com>
2024-01-22 15:03:47 +0200

    Implement Pagination

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 import Pagination from "./components/Pagination";
 
-export default function Home() {
-  return (
-      <Pagination itemCount={100} pageSize={5} currentPage={1}/>
-  )
+export default function Home({
+  searchParams,
+}: {
+  searchParams: { page: string };
+}) {
+  const currentPage = parseInt(searchParams.page, 10) || 1; // Use parseInt with base 10 and default to 1 if NaN
+  return <Pagination itemCount={100} pageSize={5} currentPage={currentPage} />;
 }
