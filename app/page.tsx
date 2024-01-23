849097b2fff54c566deb1ed434e5fa4e5e0b03ad
Markfady <markfady9@gmail.com>
2024-01-23 12:58:13 +0200

    Build the LatestIssue component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-import Pagination from "./components/Pagination";
+import LatestIssues from "@/LatestIssues";
 
-export default function Home({
-  searchParams,
-}: {
-  searchParams: { page: string };
-}) {
-  const currentPage = parseInt(searchParams.page, 10) || 1; // Use parseInt with base 10 and default to 1 if NaN
-  return <Pagination itemCount={100} pageSize={5} currentPage={currentPage} />;
+
+export default function Home() {
+  return (
+    <LatestIssues />
+  );
 }
